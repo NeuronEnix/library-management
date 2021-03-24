7ed01d2710b68480c46d3a0b49378c1857650541
NeuronEnix <kaushikrb909@gmail.com>
2021-03-24 23:07:16 +0530

    Added: Sign In View

--- api/user/router.js
+++ api/user/router.js
@@ -4,6 +4,9 @@ const validate = require( "./validation" );
 
 router.post( "/reg-cst", validate.signUp, controller.regCustomer );
 router.post( "/sign-in", validate.signIn, controller.signIn  );
+router.get( "/sign-in", ( req, res ) => {
+    return res.render( "signIn" );
+});
 // router.post( "/signOut", controller.signOut );
 
 module.exports = router;
