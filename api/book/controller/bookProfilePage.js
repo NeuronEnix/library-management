b44e4ea5eb341e8372d53bb08fb88f8e145b071f
NeuronEnix <kaushikrb909@gmail.com>
2021-04-01 14:10:14 +0530

    Added: Book History and trackers

--- api/book/controller/bookProfilePage.js
+++ api/book/controller/bookProfilePage.js
@@ -6,15 +6,23 @@ const { resRender } = require( "../../../handler").resHandler;
 module.exports = async( req, res, next) => {
     const data = await BookModel.aggregate([
         { $match: { _id: mongoose.Types.ObjectId( req.query.book_id ) } },
-        { $project: { _id:1, qty:1 } },
+        { $project: { _id:1, qty:1, author:1, title:1, edition:1 } },
         { $lookup: {
             from: "lends",
             let: { book_id: "$_id" },
             pipeline: [
-                // { $addFields: {
-                //     maxTime: "1",
-                // } },
                 { $match: { $expr: { $eq: [ "$book_id", "$$book_id" ] } } },
+                { $sort: { lent_at:-1 } },
+                { $lookup: {
+                    from: "users",
+                    let: { user_id: "$borrower_id" },
+                    pipeline: [
+                        { $match: { $expr: { $eq: [ "$_id", "$$user_id" ] } } },
+                        { $project: { name:1, _id:0 } }
+                    ],
+                    as: "userDoc"
+                }},
+                { $unwind: "$userDoc" },
                 { $group: {
                     _id: null,
                     lifeTimeBorrow: { $sum: 1 },
@@ -25,9 +33,25 @@ module.exports = async( req, res, next) => {
                             { $eq: [ "$sts", "l" ] },
                             { $lt: [ "$due_at", new Date() ] },
                         ]},
-                        1, 0 ] } },
+                        1, 0 ] 
                     }},
-                    { $project: { _id:0 } },
+                    history: { $push: { 
+                        color: {
+                            $switch: {
+                            branches: [
+                                { case: { $eq: [ "$sts", "r" ] }, then: "success" },
+                                { case: { $lt: [ "$due_at", new Date() ] }, then: "danger" },
+                            ],
+                            default: "primary",
+                            }
+                        },
+                        name: "$userDoc.name",
+                        lent_at: { $dateToString: { date: "$lent_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "NULL" } },
+                        due_at: { $dateToString: { date: "$due_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "NULL" } },
+                        ret_at: { $dateToString: { date: "$ret_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "Not Returned" } },
+                    }},
+                }},
+                { $project: { _id:0 } },
             ],
             as: "trackers",
         }},
@@ -39,9 +63,6 @@ module.exports = async( req, res, next) => {
         { $project: { _id:0, qty:0 } },
     ]);
     
-    return resRender( res, "book/bookProfilePage", {
-        ...req.query, ...data[0]
-        
-    } );
+    return resRender( res, "book/bookProfilePage", { ...data[0] } );
 
 }
