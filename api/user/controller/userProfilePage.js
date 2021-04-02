7fe15ef4ef365eb5889af7d419684eba93a89e0d
NeuronEnix <kaushikrb909@gmail.com>
2021-04-02 11:30:02 +0530

    Fixed: trackers getting limited in userProfilePage

--- api/user/controller/userProfilePage.js
+++ api/user/controller/userProfilePage.js
@@ -1,7 +1,9 @@
+const mongoose = require("mongoose");
 const UserModel = require( "../model" );
-const { resRender } = require( "../../../handler").resHandler;
 
 const { noOfBookHistoryListPerPage } = require( "../../../config").user;
+
+const { resRender } = require( "../../../handler").resHandler;
 
 module.exports = async( req, res, next) => {
 
@@ -9,14 +11,99 @@ module.exports = async( req, res, next) => {
 
     const noOfDocToBeSkipped = pg * noOfBookHistoryListPerPage;
 
+    // const userProfileData = await UserModel.aggregate([
+    //     { $match: { email:req.query.email } },
+    //     { $project: { _id:1, name:1, email:1, contact:1 } },
+    //     { $lookup: {
+    //         from: "lends",
+    //         let: { user_id: "$_id" },
+    //         pipeline: [
+    //             { $match: { $expr: { $eq: [ "$borrower_id", "$$user_id" ] } } },
+    //             { $sort: { lent_at:-1 } },
+    //             { $skip: noOfDocToBeSkipped || 0 },
+    //             { $limit: noOfBookHistoryListPerPage || 20 },
+    //             { $lookup: {
+    //                 from: "books",
+    //                 let: { book_id: "$book_id" },
+    //                 pipeline: [
+    //                     { $match: { $expr: { $eq: [ "$_id", "$$book_id" ] } } },
+    //                     { $project: { title:1, _id:0 } }
+    //                 ],
+    //                 as: "bookDoc"
+    //             }},
+    //             { $unwind: "$bookDoc" },
+    //             { $group: {
+    //                 _id: null,
+    //                 lifeTimeBorrow: { $sum: 1 },
+    //                 lifeTimeOverDue: { $sum: { $cond: [ { $gt: [ "$ret_at", "$due_at" ] }, 1, 0 ] } },
+    //                 borrowed: { $sum: { $cond: [ { $eq: [ "$sts", "l" ] }, 1, 0 ] } },
+    //                 overDue: { $sum: { $cond: [
+    //                     { $and: [
+    //                         { $eq: [ "$sts", "l" ] },
+    //                         { $lt: [ "$due_at", new Date() ] },
+    //                     ]},
+    //                     1, 0 ] 
+    //                 }},
+    //                 history: { $push: { 
+    //                     color: {
+    //                         $switch: {
+    //                         branches: [
+    //                             { case: { $eq: [ "$sts", "r" ] }, then: "success" },
+    //                             { case: { $lt: [ "$due_at", new Date() ] }, then: "danger" },
+    //                         ],
+    //                         default: "primary",
+    //                         }
+    //                     },
+    //                     title: "$bookDoc.title",
+    //                     lent_at: { $dateToString: { date: "$lent_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "NULL" } },
+    //                     due_at: { $dateToString: { date: "$due_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "NULL" } },
+    //                     ret_at: { $dateToString: { date: "$ret_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "Not Returned" } },
+    //                 }},
+    //             }},
+    //             { $project: { _id:0,  } },
+    //         ],
+    //         as: "trackers",
+    //     }},
+    //     { $unwind: {
+    //         path : "$trackers",
+    //         preserveNullAndEmptyArrays: true
+    //     } },
+    //     { $project: { _id:0 } },
+    // ]);
+
     const userProfileData = await UserModel.aggregate([
-        { $match: { email:req.query.email } },
+        { $match: { _id: mongoose.Types.ObjectId( req.query.user_id ) } },
         { $project: { _id:1, name:1, email:1, contact:1 } },
         { $lookup: {
             from: "lends",
             let: { user_id: "$_id" },
             pipeline: [
                 { $match: { $expr: { $eq: [ "$borrower_id", "$$user_id" ] } } },
+                { $project: { lent_at:1, due_at:1, ret_at:1, sts:1, _id:0 } },
+                { $group: {
+                    _id: null,
+                    lifeTimeBorrow: { $sum: 1 },
+                    lifeTimeOverDue: { $sum: { $cond: [ { $gt: [ "$ret_at", "$due_at" ] }, 1, 0 ] } },
+                    borrowed: { $sum: { $cond: [ { $eq: [ "$sts", "l" ] }, 1, 0 ] } },
+                    overDue: { $sum: { $cond: [
+                        { $and: [
+                            { $eq: [ "$sts", "l" ] },
+                            { $lt: [ "$due_at", new Date() ] },
+                        ]},
+                        1, 0 ], 
+                    }},
+                }},
+                { $project: { _id:0 } },
+            ],
+            as: "trackers",
+        }},
+        { $unwind: { path : "$trackers", preserveNullAndEmptyArrays: true } },
+        { $lookup: { 
+            from: "lends",
+            let: { user_id: "$_id" },
+            pipeline: [
+                { $match: { $expr: { $eq: [ "$borrower_id", "$$user_id" ] } } },
+                { $project: { lent_at:1, due_at:1, ret_at:1, sts:1, book_id:1, _id:0 } },
                 { $sort: { lent_at:-1 } },
                 { $skip: noOfDocToBeSkipped || 0 },
                 { $limit: noOfBookHistoryListPerPage || 20 },
@@ -32,16 +119,6 @@ module.exports = async( req, res, next) => {
                 { $unwind: "$bookDoc" },
                 { $group: {
                     _id: null,
-                    lifeTimeBorrow: { $sum: 1 },
-                    lifeTimeOverDue: { $sum: { $cond: [ { $gt: [ "$ret_at", "$due_at" ] }, 1, 0 ] } },
-                    borrowed: { $sum: { $cond: [ { $eq: [ "$sts", "l" ] }, 1, 0 ] } },
-                    overDue: { $sum: { $cond: [
-                        { $and: [
-                            { $eq: [ "$sts", "l" ] },
-                            { $lt: [ "$due_at", new Date() ] },
-                        ]},
-                        1, 0 ] 
-                    }},
                     history: { $push: { 
                         color: {
                             $switch: {
@@ -50,7 +127,7 @@ module.exports = async( req, res, next) => {
                                 { case: { $lt: [ "$due_at", new Date() ] }, then: "danger" },
                             ],
                             default: "primary",
-                            }
+                            },
                         },
                         title: "$bookDoc.title",
                         lent_at: { $dateToString: { date: "$lent_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "NULL" } },
@@ -58,18 +135,17 @@ module.exports = async( req, res, next) => {
                         ret_at: { $dateToString: { date: "$ret_at",format: "%d-%m-%Y", timezone: "+05:30", onNull: "Not Returned" } },
                     }},
                 }},
-                { $project: { _id:0,  } },
+                { $unwind: "$history" },
             ],
-            as: "trackers",
+            as: "history",
         }},
-        { $unwind: {
-            path : "$trackers",
-            preserveNullAndEmptyArrays: true
-        } },
-        { $project: { _id:0 } },
+        { $addFields: { "trackers.history" : "$history.history" } },
+        { $project: { _id:0, history:0 } },
     ]);
-    if ( !userProfileData[0].trackers ) userProfileData[0].trackers = {};
+
+    if ( !userProfileData[0].trackers ) userProfileData[0].trackers = { history:[]};
     // if ( !userProfileData.trackers ) userProfileData.trackers = { borrowed:0 };
+    userProfileData[0].user_id = req.query.user_id;
     return resRender( res, "user/userProfilePage", userProfileData[0] );
 
 }
